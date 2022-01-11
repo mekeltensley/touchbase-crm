69da8b17cc7be033ba958a5a6189ec4f5001b692
Mekel Tensley <mekelintech@gmail.com>
2022-01-11 01:00:06 -0600

    Extended templates to main file

--- leads/forms.py
+++ leads/forms.py
@@ -1,5 +1,17 @@
 # Has all forms in one file 
 from django import forms
+from .models import Lead
+
+class LeadModelForm(forms.ModelForm):
+    class Meta:
+        model = Lead
+        fields = {
+            "email",
+            "first_name",
+            "last_name",
+            "contact_rep",
+        }
+        
 
 class LeadForm(forms.Form):
     email = forms.EmailField()
--- leads/views.py
+++ leads/views.py
@@ -1,54 +1,85 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Lead, Contact_Rep
-from .forms import LeadForm
+from .forms import LeadForm, LeadModelForm
 
 
-# function based views 
+# function based views
 
-#renders list of leads
+# renders list of leads
+
 
 def lead_list(request):
-    # Returns a query set from the database 
+    # Returns a query set from the database
     # leads is  list of objects
     leads = Lead.objects.all()
-    context = {
-        "leads": leads
-    }
+    context = {"leads": leads}
     return render(request, "leads/lead_list.html", context)
 
-#displays each lead by primary key 
+
+# displays each lead by primary key
+
 
 def lead_detail(request, pk):
     lead = Lead.objects.get(id=pk)
-    context = {
-        "lead": lead
-    }
- 
+    context = {"lead": lead}
+
     return render(request, "leads/lead_detail.html", context)
+
 
 def lead_create(request):
     # when called, this will either create a new form and post form if set to POST
-    form = LeadForm()
+    form = LeadModelForm()
     if request.method == "POST":
-        print("Receiving a post request")
-        form = LeadForm(request.POST)
+        form = LeadModelForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
-            email = form.cleaned_data['email']
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            phone_number = form.cleaned_data['phone_number']
-            contact_rep = Contact_Rep.objects.first()
-            Lead.objects.create(
-                email=email,
-                first_name=first_name,
-                last_name=last_name,
-                phone_number=phone_number,
-                contact_rep=contact_rep
-            )
-            print("Lead has been created")
+            form.save()
+            return redirect("/leads")
     context = {
         "form": form
     }
     return render(request, "leads/lead_create.html", context)
+
+def lead_update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(instance=lead)
+    if request.method == "POST":
+        form = LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
+    context = {
+        "form": form,        
+        "lead": lead
+    }
+
+    return render(request, "leads/lead_update.html", context)
+
+def lead_delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/leads")
+
+
+# def lead_update(request, pk):
+#     lead = Lead.objects.get(id=pk)
+#     form = LeadForm()
+#     if request.method == "POST":
+#         form = LeadForm(request.POST)
+#         if form.is_valid():
+#             email = form.cleaned_data['email']
+#             first_name = form.cleaned_data['first_name']
+#             last_name = form.cleaned_data['last_name']
+#             phone_number = form.cleaned_data['phone_number']
+#             # Updates these field only
+#             lead.email = email
+#             lead.first_name = first_name
+#             lead.last_name = last_name
+#             lead.phone_number = phone_number
+#             lead.save()
+#             return redirect("/leads")
+#     context = {
+#         "form": form,
+#         "lead": lead
+#     }
+#     return render(request, "leads/lead_update.html", context)
