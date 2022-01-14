2f484c327d43be179714655e53329c29b0171de8
Mekel Tensley <mekelintech@gmail.com>
2022-01-14 05:51:58 -0600

    Create contact rep form to create new reps

---
+++ reps/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from leads.models import Contact_Rep
+
+class ContactRepModelForm(forms.ModelForm):
+    class Meta:
+        model = Contact_Rep
+        fields = ['user']
--- reps/views.py
+++ reps/views.py
@@ -1,6 +1,10 @@
+from audioop import reverse
+from re import template
 from django.views import generic
+from django.shortcuts import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from leads.models import Contact_Rep
+from .forms import ContactRepModelForm
 
 
 class ContactRepListView(LoginRequiredMixin, generic.ListView):
@@ -8,3 +12,10 @@ class ContactRepListView(LoginRequiredMixin, generic.ListView):
     
     def get_queryset(self):
         return Contact_Rep.objects.all()
+    
+class ContactRepCreateView(LoginRequiredMixin, generic.CreateView):
+    template_name = "reps/rep_create.html"
+    form_class = ContactRepModelForm
+    
+    def get_success_url(self):
+        return reverse("reps:rep-list")
