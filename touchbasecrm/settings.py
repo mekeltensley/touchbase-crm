e6a46faf52aaf39c69e835c9606565f2b4a456d1
Mekel Tensley <mekelintech@gmail.com>
2022-01-20 19:04:31 -0500

    Change settings

--- touchbasecrm/settings.py
+++ touchbasecrm/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'touchbasecrm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
     }
 }
 
