ea921d14eb153e3e3979098c5d96bda14c874b78
Mekel Tensley <mekelintech@gmail.com>
2022-01-20 20:55:32 -0500

    Apply migrations

--- touchbasecrm/settings.py
+++ touchbasecrm/settings.py
@@ -133,7 +133,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'leads.User'
-LOGIN_REDIRECT_URL = '/leads'
+LOGIN_REDIRECT_URL = '/reps'
 LOGIN_URL = "/login"
 LOGOUT_REDIRECT_URL = "/"
 
